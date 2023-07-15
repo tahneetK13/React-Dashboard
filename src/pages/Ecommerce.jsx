43a4dad4e7b0e088781cb1d575d777662e49ab12
Tahneet Kanwal <99545467+tahneetK13@users.noreply.github.com>
2023-07-15 16:43:20 +0530

    Update Ecommerce.jsx

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-//import { BsCurrencyDollar } from 'react-icons/bs';
-//import { GoPrimitiveDot } from 'react-icons/go';
 import { IoIosMore } from 'react-icons/io';
 import { DropDownListComponent } from '@syncfusion/ej2-react-dropdowns';
 
